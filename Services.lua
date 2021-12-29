e59f0910d9e0680636f23ed166ea5dc37750f9a9
NUTTREX <94196871+NUTTREX@users.noreply.github.com>
2021-12-29 18:00:37 +0100

    More Services
    Added further services to the table.

--- Services.lua
+++ Services.lua
@@ -1,5 +1,16 @@
 local Services = {}
 
+Services.MarketplaceService = game:GetService("MarketplaceService")
+Services.CollectionService = game:GetService("CollectionService")
+Services.ReplicatedStorage = game:GetService("ReplicatedStorage")
+Services.UserInputService = game:GetService("UserInputService")
+Services.TweenService = game:GetService("TweenService")
+Services.HttpService = game:GetService("HttpService")
+Services.RunService = game:GetService("RunService")
+Services.WorkSpace = game:GetService("Workspace")
+Services.Lighting = game:GetService("Lighting")
 Services.Players = game:GetService('Players')
+Services.Debris = game:GetService('Debris')
+Services.Player = Players.LocalPlayer
 
 return Services
