6567e49793aabd9b68490325df03c8613784fee8
NUTTREX <94196871+NUTTREX@users.noreply.github.com>
2021-12-30 16:46:27 +0100

    Create Functions.lua
    Common Functions

---
+++ Functions.lua
@@ -0,0 +1,48 @@
+--[[
+             /$$   /$$ /$$   /$$ /$$$$$$$$ /$$$$$$$  /$$$$$$$$ /$$   /$$
+            | $$$ | $$| $$  | $$|__  $$__/| $$__  $$| $$_____/| $$  / $$
+            | $$$$| $$| $$  | $$   | $$   | $$  \ $$| $$      |  $$/ $$/
+            | $$ $$ $$| $$  | $$   | $$   | $$$$$$$/| $$$$$    \  $$$$/ 
+            | $$  $$$$| $$  | $$   | $$   | $$__  $$| $$__/     >$$  $$ 
+            | $$\  $$$| $$  | $$   | $$   | $$  \ $$| $$       /$$/\  $$
+            | $$ \  $$|  $$$$$$/   | $$   | $$  | $$| $$$$$$$$| $$  \ $$
+            |__/  \__/ \______/    |__/   |__/  |__/|________/|__/  |__/
+--]]
+
+local Services = loadstring(game:HttpGet('https://raw.githubusercontent.com/NUTTREX/Roblox/main/Services.lua'))()
+
+local Functions = {}
+
+Functions.Generate_String = function(Number)
+    local String
+    for Index = 0, Number, 1 do
+        String = String..tostring(utf8.char(math.random(63744, 64109)))
+    end
+    return String
+end
+
+Functions.Create_Tween = function(Object, Goal, Direction, Style, Time, WaitForTween)
+    Style = Style or {}
+    local Tween = Services.TweenService:Create(Object, TweenInfo.new(Time, Style, Direction), Goal)
+    Tween:Play()
+    if WaitForTween then
+        Tween.Completed:wait()
+    end
+end
+
+Functions.Create_Sound = function(Object, Volume, WaitForSound)
+    local Sound = Instance.new("Sound")
+    Sound.Parent = Services.CoreGui
+    Sound.Volume = Volume or 1
+    Sound.SoundId = getsynasset(Object)
+    Sound:Play()
+
+	if WaitForSound then
+		Sound.Ended:Wait()
+        Services.Debris:AddItem(Sound, 1)
+	end
+    task.spawn(function()
+        Sound.Ended:Wait()
+        Services.Debris:AddItem(Sound, 1)
+    end)
+end
